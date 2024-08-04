91f7c84a4bfdccc1dad595c945b164f9488bbcf0
Devendra Suryavanshi <devendrasooryavanshee@gmail.com>
2024-08-04 18:00:22 +0530

    fised issue: nextUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config = {
     // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   prefix: "",
